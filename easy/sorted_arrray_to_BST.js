1e081e4ed21ef877db9e1d17637fe24878082126
jibin <jibinkj009@hotmail.com>
2026-01-20 12:30:47 +0000

    BST from sorted array

---
+++ easy/sorted_arrray_to_BST.js
@@ -0,0 +1,27 @@
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function (nums) {
+  const build = (left, right) => {
+    if (left > right) return null;
+
+    const mid = Math.floor((left + right) / 2);
+    const node = new TreeNode(nums[mid]);
+
+    node.left = build(left, mid - 1);
+    node.right = build(mid + 1, right);
+
+    return node;
+  };
+
+  return build(0, nums.length - 1);
+};
+
+console.log("result", sortedArrayToBST([1, 3]));

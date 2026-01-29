fc80a98d7a842dc7f30d22a9fa951aa6ade4acf1
jibin <jibinkj009@hotmail.com>
2026-01-29 11:06:18 +0000

    Excel col number

---
+++ easy/excel_col_number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} columnTitle
+ * @return {number}
+ */
+var titleToNumber = function (columnTitle) {
+  let result = 0;
+
+  for (let i = 0; i < columnTitle.length; i++) {
+    const value = columnTitle.charCodeAt(i) - 64;
+    result = result * 26 + value;
+  }
+
+  return result;
+};
+
+console.log(titleToNumber("ZY"));

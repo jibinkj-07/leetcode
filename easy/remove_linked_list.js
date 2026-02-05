5bb5c2d771e6e31603c995f62fba2ae9ceeb958a
jibin <jibinkj009@hotmail.com>
2026-02-05 15:13:00 +0000

    remove node from linked list

---
+++ easy/remove_linked_list.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function (head, val) {
+  const dummy = new ListNode(0);
+  dummy.next = head;
+
+  let current = dummy;
+
+  while (current.next !== null) {
+    if (current.next.val === val) {
+      current.next = current.next.next; // remove node
+    } else {
+      current = current.next; // move forward
+    }
+  }
+
+  return dummy.next;
+};
